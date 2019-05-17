85976f97e9096b637ddb9f907298c399ba109be7
Naman Manglik <CORPORATE\naman@lptp-15.corporate.phonon.in>
2019-05-17 11:29:44 +0530

    fix sip_call script

--- sip_call.sh
+++ sip_call.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+
+# Need to change the path
+
 dir_path=/var/spool/asterisk/outgoing/
+dir_path=/home/local/CORPORATE/naman/work/SIPp/SIPp/test_dir/
+sipp_dir=/home/ubuntu/sippCall/
+
 if [ $1 != "" ] || [ $2 != "" ] || [ $3 != "" ]
 then
 	for ((a=1;a<=$1;a++));
 	do
-		for ((i=1;i<=5;i++));
+		for ((i=1;i<=$2;i++));
     	 	do
-   	 	`cp "/home/ubuntu/sipCalls/sipp.call"  "/home/ubuntu/sipCalls/sipp3_"$a"_"$i".call"`
-   	 	`cp "/home/ubuntu/sipCalls/sipp3_"$a"_"$i".call" $dir_path`
+		echo $sipp_dir"sipp.call"
+   	 	`cp $sipp_dir"sipp.call"  $sipp_dir"sipp3_"$a"_"$i".call"`
+   	 	`cp $sipp_dir"sipp3_"$a"_"$i".call" $dir_path`
 		done
 		
 	 	printf "\n----- $a iteraton ------"`date +%d%m%y_%H%M%S`
 	 	printf "\ncycle completed for $2 simultanious calls"
-	 	printf "\n\n sleeping for $2 seconds ... \n"
-    			 sleep $2;
+	 	printf "\n\n sleeping for $3 seconds ... \n"
+    			 sleep $3;
 done
 else
 	printf "\n Please run with three arguments:\n\n 1. Number of iterations\n 2. Number of simultanious calls\n 3. sleep seconds\n" 
